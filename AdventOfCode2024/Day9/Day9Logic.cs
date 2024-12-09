2cf5ded32b9519deb2f0b0f4fde74de7219c6c35
CezaryMichalczyk <AEVEN-ADMIN-CEMI@dnpoctenant.onmicrosoft.com>
2024-12-09 18:00:31 +0100

    Add Day9 empty frame

---
+++ AdventOfCode2024/Day9/Day9Logic.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode2024.Interfaces;
+using System.Text;
+
+namespace AdventOfCode2024.Day9
+{
+    public class Day9Logic : IPuzzles
+    {
+        private const string fileName = "day9_Input_test.in";
+        private const Int32 BufferSize = 128;
+
+        public string FirstPuzzle()
+        {
+            int result = 0;
+
+            using (var fileStream = File.OpenRead(fileName))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                string? line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public string SecondPuzzle()
+        {
+            int result = 0;
+
+            using (var fileStream = File.OpenRead(fileName))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                string? line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
