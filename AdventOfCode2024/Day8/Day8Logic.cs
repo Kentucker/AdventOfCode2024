db63b5f73a2bf8cab9078e6d9de70270a266e0a1
CezaryMichalczyk <AEVEN-ADMIN-CEMI@dnpoctenant.onmicrosoft.com>
2024-12-09 17:26:22 +0100

    day8p1v1

--- AdventOfCode2024/Day8/Day8Logic.cs
+++ AdventOfCode2024/Day8/Day8Logic.cs
@@ -10,19 +10,34 @@ namespace AdventOfCode2024.Day8
 
         public string FirstPuzzle()
         {
-            int result = 0;
+            List<(char, Point)> antennas = [];
+            HashSet<Point> antinodes = [];
+            var mapSize = new Point();
 
             using (var fileStream = File.OpenRead(fileName))
             using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
             {
                 string? line;
+                var lineNumber = 0;
                 while ((line = streamReader.ReadLine()) != null)
                 {
+                    mapSize = new Point(lineNumber, line.Length-1);
 
+                    for (int i = 0; i<line.Length; i++)
+                    {
+                        if (line[i] != '.')
+                        {
+                            antennas.Add((line[i], new Point(lineNumber, i)));
+                        }
+                    }
+
+                    lineNumber++;
                 }
             }
 
-            return result.ToString();
+            antinodes = SeaechForAntinodes(antennas, mapSize);
+
+            return antinodes.Count.ToString();
         }
 
         public string SecondPuzzle()
@@ -40,6 +55,94 @@ namespace AdventOfCode2024.Day8
             }
 
             return result.ToString();
+        }
+
+        private HashSet<Point> SeaechForAntinodes(List<(char, Point)> antennas, Point maxMapSize)
+        {
+            var antinodes = new HashSet<Point>();
+
+            var distinctAntennasTypes = antennas.Select(x => x.Item1).Distinct();
+
+            foreach (var antennaType in distinctAntennasTypes)
+            {
+                var antennasOfASingleTypePositions = antennas.Where(x => x.Item1 == antennaType).Select(x => x.Item2).ToList();
+
+                foreach (var antenna in antennasOfASingleTypePositions)
+                {
+                    for (int i = 0; i < antennasOfASingleTypePositions.Count - 1; i++)
+                    {
+                        for(int j = i + 1; j < antennasOfASingleTypePositions.Count; j++)
+                        {
+                            var diffX = antennasOfASingleTypePositions[i].X - antennasOfASingleTypePositions[j].X;
+                            var diffY = antennasOfASingleTypePositions[i].Y - antennasOfASingleTypePositions[j].Y;
+
+                            var point1 = new Point();
+                            var point2 = new Point();
+
+                            if (diffX * diffY > 0)
+                            {
+                                point1 = new Point(
+                                    Math.Min(antennasOfASingleTypePositions[i].X, antennasOfASingleTypePositions[j].X) - Math.Abs(diffX),
+                                    Math.Min(antennasOfASingleTypePositions[i].Y, antennasOfASingleTypePositions[j].Y) - Math.Abs(diffY)
+                                    );
+                                point2 = new Point(
+                                    Math.Max(antennasOfASingleTypePositions[i].X, antennasOfASingleTypePositions[j].X) + Math.Abs(diffX),
+                                    Math.Max(antennasOfASingleTypePositions[i].Y, antennasOfASingleTypePositions[j].Y) + Math.Abs(diffY)
+                                    );
+                            }
+                            else
+                            {
+                                point1 = new Point(
+                                    Math.Min(antennasOfASingleTypePositions[i].X, antennasOfASingleTypePositions[j].X) - Math.Abs(diffX),
+                                    Math.Max(antennasOfASingleTypePositions[i].Y, antennasOfASingleTypePositions[j].Y) + Math.Abs(diffY)
+                                    );
+                                point2 = new Point(
+                                    Math.Max(antennasOfASingleTypePositions[i].X, antennasOfASingleTypePositions[j].X) + Math.Abs(diffX),
+                                    Math.Min(antennasOfASingleTypePositions[i].Y, antennasOfASingleTypePositions[j].Y) - Math.Abs(diffY)
+                                    );
+                            }
+
+                            if(IsPointInMap(point1, maxMapSize))
+                            {
+                                antinodes.Add(point1);
+                            }
+
+                            if (IsPointInMap(point2, maxMapSize))
+                            {
+                                antinodes.Add(point2);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return antinodes;
+        }
+
+        private static bool IsPointInMap(Point point, Point maxMapSize)
+        {
+            if(point.X >=0 && point.X <= maxMapSize.X && point.Y >= 0 && point.Y <= maxMapSize.Y)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private struct Point
+        {
+            public Point()
+            {  
+            }
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X;
+            public int Y;
         }
     }
 }
