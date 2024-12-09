987150f0b5c11ea2c39389312ba326314f8bb21f
CezaryMichalczyk <AEVEN-ADMIN-CEMI@dnpoctenant.onmicrosoft.com>
2024-12-09 17:47:13 +0100

    Day8P2

--- AdventOfCode2024/Day8/Day8Logic.cs
+++ AdventOfCode2024/Day8/Day8Logic.cs
@@ -7,13 +7,11 @@ namespace AdventOfCode2024.Day8
     {
         private const string fileName = "day8_Input.in";
         private const Int32 BufferSize = 128;
+        private List<(char, Point)> antennas = [];
+        private Point mapSize = new();
 
         public string FirstPuzzle()
         {
-            List<(char, Point)> antennas = [];
-            HashSet<Point> antinodes = [];
-            var mapSize = new Point();
-
             using (var fileStream = File.OpenRead(fileName))
             using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
             {
@@ -35,29 +33,19 @@ namespace AdventOfCode2024.Day8
                 }
             }
 
-            antinodes = SeaechForAntinodes(antennas, mapSize);
+            var antinodes = SearchForAntinodes(antennas, mapSize);
 
             return antinodes.Count.ToString();
         }
 
         public string SecondPuzzle()
         {
-            int result = 0;
+            var antinodes = SearchForAntinodesAtAnyGridPosition(antennas, mapSize);
 
-            using (var fileStream = File.OpenRead(fileName))
-            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
-            {
-                string? line;
-                while ((line = streamReader.ReadLine()) != null)
-                {
-
-                }
-            }
-
-            return result.ToString();
+            return antinodes.Count.ToString();
         }
 
-        private HashSet<Point> SeaechForAntinodes(List<(char, Point)> antennas, Point maxMapSize)
+        private static HashSet<Point> SearchForAntinodes(List<(char, Point)> antennas, Point maxMapSize)
         {
             var antinodes = new HashSet<Point>();
 
@@ -82,19 +70,72 @@ namespace AdventOfCode2024.Day8
                                 antennasOfASingleTypePositions[i].X + vector.X,
                                 antennasOfASingleTypePositions[i].Y + vector.Y
                                 );
-                            var point2 = new Point(
-                                antennasOfASingleTypePositions[j].X - vector.X,
-                                antennasOfASingleTypePositions[j].Y - vector.Y
-                                );
 
                             if (IsPointInMap(point1, maxMapSize))
                             {
                                 antinodes.Add(point1);
                             }
 
+                            var point2 = new Point(
+                                antennasOfASingleTypePositions[j].X - vector.X,
+                                antennasOfASingleTypePositions[j].Y - vector.Y
+                                );
+
                             if (IsPointInMap(point2, maxMapSize))
                             {
                                 antinodes.Add(point2);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return antinodes;
+        }
+
+        private static HashSet<Point> SearchForAntinodesAtAnyGridPosition(List<(char, Point)> antennas, Point maxMapSize)
+        {
+            var antinodes = new HashSet<Point>();
+
+            var distinctAntennasTypes = antennas.Select(x => x.Item1).Distinct();
+
+            foreach (var antennaType in distinctAntennasTypes)
+            {
+                var antennasOfASingleTypePositions = antennas.Where(x => x.Item1 == antennaType).Select(x => x.Item2).ToList();
+
+                foreach (var antenna in antennasOfASingleTypePositions)
+                {
+                    for (int i = 0; i < antennasOfASingleTypePositions.Count - 1; i++)
+                    {
+                        for (int j = i + 1; j < antennasOfASingleTypePositions.Count; j++)
+                        {
+                            var diffX = antennasOfASingleTypePositions[i].X - antennasOfASingleTypePositions[j].X;
+                            var diffY = antennasOfASingleTypePositions[i].Y - antennasOfASingleTypePositions[j].Y;
+
+                            var vector = new Point(diffX, diffY);
+
+                            var currentPoint = antennasOfASingleTypePositions[i];
+
+                            while (IsPointInMap(currentPoint, maxMapSize))
+                            {
+                                antinodes.Add(currentPoint);
+
+                                currentPoint = new Point(
+                                    currentPoint.X + vector.X,
+                                    currentPoint.Y + vector.Y
+                                );
+                            }
+
+                            currentPoint = antennasOfASingleTypePositions[j];
+
+                            while (IsPointInMap(currentPoint, maxMapSize))
+                            {
+                                antinodes.Add(currentPoint);
+
+                                currentPoint = new Point(
+                                    currentPoint.X - vector.X,
+                                    currentPoint.Y - vector.Y
+                                );
                             }
                         }
                     }
