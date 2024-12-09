625a22d2c1e2628d241e345fcf65135498e84ebf
CezaryMichalczyk <AEVEN-ADMIN-CEMI@dnpoctenant.onmicrosoft.com>
2024-12-09 20:43:47 +0100

    Day9p1

--- AdventOfCode2024/Day9/Day9Logic.cs
+++ AdventOfCode2024/Day9/Day9Logic.cs
@@ -5,24 +5,51 @@ namespace AdventOfCode2024.Day9
 {
     public class Day9Logic : IPuzzles
     {
-        private const string fileName = "day9_Input_test.in";
+        private const string fileName = "day9_Input.in";
         private const Int32 BufferSize = 128;
+        private List<int> individualBlocks = [];
 
         public string FirstPuzzle()
         {
-            int result = 0;
+            long checksum = 0;
 
             using (var fileStream = File.OpenRead(fileName))
             using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
             {
                 string? line;
+                var ID = 0;
+
                 while ((line = streamReader.ReadLine()) != null)
                 {
+                    
 
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            individualBlocks.AddRange(Enumerable.Repeat(ID, int.Parse(line[i].ToString())));
+                            ID++;
+                        }
+                        else
+                        {
+                            individualBlocks.AddRange(Enumerable.Repeat(-1, int.Parse(line[i].ToString())));
+                        }
+                    }
+
+                    var fileFragmentation = new List<int>(individualBlocks);
+                    fileFragmentation.FileSystemFragmentation();
+
+                    var index = 0;
+
+                    while(fileFragmentation[index] != -1)
+                    {
+                        checksum += fileFragmentation[index] * index;
+                        index++;
+                    }
                 }
             }
 
-            return result.ToString();
+            return checksum.ToString();
         }
 
         public string SecondPuzzle()
@@ -40,6 +67,27 @@ namespace AdventOfCode2024.Day9
             }
 
             return result.ToString();
+        }
+    }
+
+    public static class StringExtensions
+    {
+        private static void Swap<T>(IList<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+        }
+
+        public static void FileSystemFragmentation(this List<int> individualBlocks)
+        {
+            var indexOfFirstDotOccurence = individualBlocks.IndexOf(-1);
+            while (!individualBlocks.Skip(indexOfFirstDotOccurence).All(n => n == -1))
+            {
+                var indexOfLastNumOccurence = individualBlocks.LastIndexOf(individualBlocks.LastOrDefault(x => x != -1));
+                Swap(individualBlocks, indexOfFirstDotOccurence, indexOfLastNumOccurence);
+                indexOfFirstDotOccurence = individualBlocks.IndexOf(-1);
+            }
         }
     }
 }
